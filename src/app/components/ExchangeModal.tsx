c993300df80bdcd0036ef3aabc5808953881dbb8
piyushjha0409 <peeyush0409@gmail.com>
2023-09-15 14:34:33 +0530

    imporved the design some more layers to finish

--- src/app/components/ExchangeModal.tsx
+++ src/app/components/ExchangeModal.tsx
@@ -15,6 +15,7 @@ import {
   InputLeftAddon,
   InputRightAddon,
 } from "@chakra-ui/react";
+import { FaBitcoin, FaEthereum } from "react-icons/fa";
 import { useState } from "react";
 
 type Props = {};
@@ -32,57 +33,78 @@ const ExchangeModal = (props: Props) => {
     <>
       <Button onClick={handleOpen}>Swap tokens</Button>
 
-      <Modal isOpen={isOpen} onClose={handleClose} w={22} h={22}>
+      <Modal isOpen={isOpen} onClose={handleClose}>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent maxW="700px" maxH="900px">
           <ModalHeader>
             <Center>Add exchange details</Center>
           </ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl>
-              {/* <FormLabel>You Send</FormLabel> */}
+          <ModalBody pb={6} my={4} mx={4}>
+            <FormControl my={4}>
               <InputGroup size="sm">
-                <InputLeftAddon 
-                h={12}
-                children="You Send"
-                />
+                <InputLeftAddon h={12} w={28}>
+                  <span>You Send</span>
+                </InputLeftAddon>
 
-                <Input 
-                h={12}
-                placeholder="0.0" 
-                value={token1Amount}
-                onChange={(e) => setToken1Amount(e.target.value) }
+                <Input
+                  h={12}
+                  placeholder="0.0"
+                  value={token1Amount}
+                  onChange={(e) => setToken1Amount(e.target.value)}
                 />
-                <InputRightAddon
-                h={12}
-                />
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    paddingLeft: "8px",
+                  }}
+                >
+                  <FaBitcoin size="1.5em" />
+                  <span style={{ paddingLeft: "12px" }}> BTC</span>
+                </div>
               </InputGroup>
             </FormControl>
-            <FormControl mt={4}>
-            <InputGroup size="sm">
-                <InputLeftAddon 
-                h={12}
-                children="You got"
-                />
+            <FormControl my={4}>
+              <InputGroup size="sm">
+                <InputLeftAddon h={12} w={28}>
+                  <span>You Got</span>
+                </InputLeftAddon>
 
-                <Input 
-                h={12}
-                placeholder="0.0" 
-                value={token2Amount}
-                onChange={(e) => setToken2Amount(e.target.value) }
+                <Input
+                  h={12}
+                  placeholder="0.0"
+                  value={token2Amount}
+                  onChange={(e) => setToken2Amount(e.target.value)}
                 />
-                <InputRightAddon
-                h={12}
-                />
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    paddingLeft: "8px",
+                  }}
+                >
+                  <FaEthereum size="1.5em" />
+                  <span style={{ paddingLeft: "12px" }}> ETH</span>
+                </div>
               </InputGroup>
             </FormControl>
-            <FormControl mt={8}>
+            <FormControl my={8}>
               <FormLabel>
                 <h1>Enter the wallet address</h1>
                 <Input
-                mt={2}
-                  placeholder="0x..."
+                  mt={2}
+                  w="90%"
+                  h="56px"
+                  placeholder="The recipient's ethereum address"
+                  sx={{
+                    "::placeholder": {
+                      position: "relative",
+                      top: "-14px",
+                      fontSize: "12px",
+                      fontWeight: "bold", // Adjust this value as needed
+                    },
+                  }}
                   value={walletAddress}
                   onChange={(e) => setWalletAddress(e.target.value)}
                 />
@@ -91,7 +113,7 @@ const ExchangeModal = (props: Props) => {
           </ModalBody>
           <Button colorScheme="blue" mr={3} ml={3} mb={3} onClick={handleClose}>
             Swap
-          </Button> 
+          </Button>
         </ModalContent>
       </Modal>
     </>
