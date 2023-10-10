7917afa9c96d2141707d689920c93d3b42e1ed7b
piyushjha0409 <73685420+piyushjha0409@users.noreply.github.com>
2023-10-10 14:20:45 +0530

    added icons

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -15,62 +15,64 @@ import {
   TableContainer,
   Flex,
 } from "@chakra-ui/react";
-import {HiDownload} from "react-icons/hi"
+import { HiDownload } from "react-icons/hi";
+import { CiFilter } from "react-icons/ci";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <Flex justifyContent="center">
-    <Box w="50%">
-      <Text textAlign="center">Transactions page</Text>
-      {/* There will be two buttons here  */}
-      <Box display="flex" justifyContent="space-between">
-        <Button>
+      <Box w="50%">
+        <Text textAlign="center">Transactions page</Text>
+        {/* There will be two buttons here  */}
+        <Box display="flex" justifyContent="space-between">
+          <Button alignItems="center">
+            <CiFilter />
             Add Filter
-        </Button>
-        <Button>
+          </Button>
+          <Button alignItems="center">
+            <HiDownload />
             Export
-        </Button>
-
+          </Button>
+        </Box>
+        <TableContainer alignSelf="center">
+          <Table variant="simple" size="sm">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+              <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+                <Td isNumeric>0.91444</Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
       </Box>
-      <TableContainer alignSelf='center'>
-        <Table variant="simple" size="sm">
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>inches</Td>
-              <Td>millimetres (mm)</Td>
-              <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-              <Td>feet</Td>
-              <Td>centimetres (cm)</Td>
-              <Td isNumeric>30.48</Td>
-            </Tr>
-            <Tr>
-              <Td>yards</Td>
-              <Td>metres (m)</Td>
-              <Td isNumeric>0.91444</Td>
-            </Tr>
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </TableContainer>
-    </Box>
     </Flex>
   );
 };
