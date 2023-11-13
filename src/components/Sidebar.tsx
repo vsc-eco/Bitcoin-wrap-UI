62ae7a78e239fef56596d76653b0c17b1e52a038
piyushjha0409 <73685420+piyushjha0409@users.noreply.github.com>
2023-11-13 15:25:13 +0530

    Optimization in margins in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,56 +1,55 @@
-"use client"
 import React from "react";
 import { Box, VStack, Text, Flex } from "@chakra-ui/react";
 import { AiOutlineHome } from "react-icons/ai";
 import { TfiMenuAlt } from "react-icons/tfi";
-import Image from "next/image"
+import Image from "next/image";
 
 import { AccountContext, useAccountContext } from "../context/AccountContext";
 import { useContext } from "react";
 import { ResolveUsername } from "../hooks/Hive";
 
+const Sidebar = ({ handleExchangeOnClick, handleTransactionOnClick }) => {
+  const { triggerLoginWithHive } = useAccountContext();
+  const ac = useContext(AccountContext);
 
-const Sidebar = ({ handleExchangeOnClick, handleTransactionOnClick}) => {
+  const did = ResolveUsername("vaultec");
+  console.log(did);
 
-  const {
-    triggerLoginWithHive
-  } = useAccountContext()
-  const ac = useContext(AccountContext)
-
-  const did = ResolveUsername("vaultec")
-  console.log(did)
-
-  //function for clicking the transactions 
+  //function for clicking the transactions
   return (
     <Box
-      w={["180px"]}
-      h="71vh"
+      w={["150px"]}
+      h={["65vh","65vh","65vh","71vh"]}
       boxShadow="0 0 10px rgba(0, 0, 0, 0.2)"
       borderRadius={8}
       bgColor="white"
+      paddingY="10px" // Add margin to the entire sidebar
     >
-      <VStack spacing={4} align="start" marginLeft={[".5rem",".5rem",".5rem",".5rem"]} py={5}>
-        <Flex alignItems="center" justifyContent="space-between">
-            <Flex alignItems="center" _hover={{color: "blue.500"}} >
-              <AiOutlineHome />
-              <Text ml={2} fontSize="lg" fontWeight="bold" onClick={handleExchangeOnClick} style={{ cursor:"pointer"}}> 
-                Home
-              </Text>
-            </Flex>
+      <VStack align="start" >
+        <Flex alignItems="center" justifyContent="space-between" marginX="10px">
+          {/* Add margin to this Flex container */}
+          <Flex alignItems="center" _hover={{ color: "blue.500" }}>
+            <AiOutlineHome />
+            <Text ml={2} fontSize="lg" fontWeight="bold" onClick={handleExchangeOnClick} style={{ cursor: "pointer" }}>
+              Home
+            </Text>
+          </Flex>
         </Flex>
-        <Flex alignItems="center" justifyContent="space-between">
-            <Flex alignItems="center" _hover={{color: "blue.500"}} onClick={handleTransactionOnClick}>
-              <TfiMenuAlt />
-              <Text ml={2} fontSize="lg" fontWeight="bold" style={{ cursor:"pointer"}}>
-                Transactions
-              </Text>
-            </Flex>
+        <Flex alignItems="center" justifyContent="space-between" marginX="10px">
+          {/* Add margin to this Flex container */}
+          <Flex alignItems="center" _hover={{ color: "blue.500" }} onClick={handleTransactionOnClick}>
+            <TfiMenuAlt />
+            <Text ml={1} fontSize="lg" fontWeight="bold" style={{ cursor: "pointer" }}>
+              Transactions
+            </Text>
+          </Flex>
         </Flex>
-        <Box>
-          <Text fontSize='smaller'>Sign in with:</Text>
-        <Flex bgColor="#1c1b1b" _hover={{bgColor:"black"}} p={2} borderRadius={3}>
-        <Image src="/keychain.png" height={112} width={112} alt="keychain logo" onClick={triggerLoginWithHive} style={{cursor:"pointer"}} />
-        </Flex>
+        <Box marginX="10px">
+          {/* Add margin to this Box */}
+          <Text fontSize="large" textAlign="center" fontWeight="bolder">Sign in with:</Text>
+          <Flex bgColor="#1c1b1b" _hover={{ bgColor: "black" }} p={2} borderRadius={3}>
+            <Image src="/keychain.png" height={112} width={115} alt="keychain logo" onClick={triggerLoginWithHive} style={{ cursor: "pointer" }} />
+          </Flex>
         </Box>
       </VStack>
     </Box>
@@ -58,4 +57,3 @@ const Sidebar = ({ handleExchangeOnClick, handleTransactionOnClick}) => {
 };
 
 export default Sidebar;
-
