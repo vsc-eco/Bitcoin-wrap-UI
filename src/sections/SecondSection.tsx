79aafa47c283c4c99db9628af0a94985b18c40a2
piyush <peeyush0409@gmail.com>
2023-11-21 12:53:25 +0530

    changes applied

--- src/sections/SecondSection.tsx
+++ src/sections/SecondSection.tsx
@@ -6,44 +6,53 @@ import Sidebar from "../components/Sidebar";
 import Transaction from "../transactions/Transaction";
 import Hamburger from "../components/Hamburger";
 
+//export the context file 
+import { useShowComponent } from "../context/ShowComponent"
+
 type Props = {};
 
 const SecondSection = (props: Props) => {
-  const [showTransaction, setShowTransaction] = useState(true);
-  const [showExchangeModal, setShowExchangeModal] = useState(false);
+    const [showTransaction, setShowTransaction] = useState(true);
+    const [showExchangeModal, setShowExchangeModal] = useState(false);
 
-  //function for handling the TransactionModal
-  const handleTransactionOnClick = () => {
-    setShowTransaction(true);
-    setShowExchangeModal(false);
-  };
+    //function for handling the TransactionModal
+    const handleTransactionOnClick = () => {
+        setShowTransaction(true);
+        setShowExchangeModal(false);
+    };
 
-  //function for handling the TransactionModal
-  const handleExchangeOnClick = () => {
-    setShowTransaction(false);
-    setShowExchangeModal(true);
-  };
+    //function for handling the TransactionModal
+    const handleExchangeOnClick = () => {
+        setShowTransaction(false);
+        setShowExchangeModal(true);
+    };
 
-  return (
-    // <Flex w="100%" h="90vh" justifyContent="center">
-    //   <Flex w="80%">
-    //     <Flex w="100%" justifyContent="center" >
-    //       <Flex display={["none", "none", "none", "block"]} px={12} alignItems="center">
-    // <Sidebar
-    //   handleExchangeOnClick={handleExchangeOnClick}
-    //   handleTransactionOnClick={handleTransactionOnClick}
-    // />
-    //       </Flex>
-    //       <Flex display={["block", "block", "block", "none"]} px={4}>
-    //         <Hamburger />
-    //       </Flex>
-          // <Flex>{showTransaction ? <Transaction /> : <ExchangeModal />}</Flex>
-    //     </Flex>
-    //   </Flex>
-    // </Flex>
+    const { showComponent } = useShowComponent();
 
-    //updates only for desktop view no mobile view from now
-    <Flex w="100%" h="90vh">
+    if (showComponent) {
+        return null
+    }
+
+    return (
+        // <Flex w="100%" h="90vh" justifyContent="center">
+        //   <Flex w="80%">
+        //     <Flex w="100%" justifyContent="center" >
+        //       <Flex display={["none", "none", "none", "block"]} px={12} alignItems="center">
+        // <Sidebar
+        //   handleExchangeOnClick={handleExchangeOnClick}
+        //   handleTransactionOnClick={handleTransactionOnClick}
+        // />
+        //       </Flex>
+        //       <Flex display={["block", "block", "block", "none"]} px={4}>
+        //         <Hamburger />
+        //       </Flex>
+        // <Flex>{showTransaction ? <Transaction /> : <ExchangeModal />}</Flex>
+        //     </Flex>
+        //   </Flex>
+        // </Flex>
+
+        //updates only for desktop view no mobile view from now
+        <Flex w="100%" h="90vh">
       <Flex w="30%" id="sidebar" alignItems="center" justifyContent="end" mx={10}>
         <Sidebar
           handleExchangeOnClick={handleExchangeOnClick}
@@ -54,7 +63,9 @@ const SecondSection = (props: Props) => {
       {showTransaction ? <Transaction /> : <ExchangeModal />}
       </Flex>
     </Flex>
-  );
+    );
+
+
 };
 
 export default SecondSection;
