978fa826323473602824b45d1d907ad53ac96f6c
piyush <peeyush0409@gmail.com>
2023-12-01 02:19:00 +0530

    Made theme to default

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,14 +3,14 @@
 import { extendTheme } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import {
   useQuery,
   useMutation,
   useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
+} from "@tanstack/react-query";
 
 import { AccountContext } from "../context/AccountContext";
 
@@ -19,24 +19,24 @@ import "@fontsource/ia-writer-quattro/400.css";
 import "@fontsource/open-sans/700.css";
 import { client } from "../apollo/client";
 
-const colors = {
-  brand: {
-    50: "#fffdfc",
-    100: "#cce4f6", // Lighter Blue
-    500: "#4099ed ", // Normal Blue
-    900: "#001a33", // Darker Blue
-  },
-};
-
 export const theme = extendTheme({
-  colors,
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    gray: {
+      50: "#f7fafc",
+      // ...
+      900: "#171923",
+    },
+  },
   fonts: {
     heading: `'Open Sans', sans-serif`,
     body: `'ia-writer-quattro', sans-serif`,
   },
 });
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
