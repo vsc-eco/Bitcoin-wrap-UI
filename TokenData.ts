4a3b63c5a2dcd61dda98a825c7c54cd71ff85aec
piyush <peeyush0409@gmail.com>
2023-12-15 23:04:59 +0530

    feature: search token modal is completed with all details

--- TokenData.ts
+++ TokenData.ts
@@ -1,39 +1,43 @@
-import next from "next";
 
 export const tokens = [
   {
     id: 1,
     tokenName: "RAY",
+    fullname: "Radium",
     price: 50.0,
     image: "radium.jpg",
-    balanceAddr: "sasdasasda"
+    balanceAddr: "0xAb5801a7D398351b"
   },
   {
     id: 2,
     tokenName: "SOL",
+    fullname: "Solana",
     price: 50000.0,
     image: "solana.png",
-    balanceAddr: "sasasasas"
+    balanceAddr: "0xAb5801a7D398351b"
   },
   {
     id: 3,
     tokenName: "USDT",
+    fullname: "USDT",
     price: 3000.0,
     image: "tether.png",
-    balanceAddr: "sasasasas"
+    balanceAddr: "0xAb5801a7D398351b"
   },
   {
     id: 4,
     tokenName: "USDC",
+    fullname: "USDC",
     price: 3000.0,
     image: "coin.png",
-    balanceAddr: "sasasasa"
+    balanceAddr: "0xAb5801a7D398351b"
   },
   {
     id: 5,
     tokenName: "ETH",
+    fullname: "Ethereum",
     price: 3000.0,
     image: "ethereum.png",
-    balanceAddr: "sasasas"
+    balanceAddr: "0xAb5801a7D398351b"
   },
 ];
