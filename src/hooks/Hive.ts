95ca2ec2f9d551731f11543da2bd1d9ae4a4f8d7
piyush <peeyush0409@gmail.com>
2024-02-13 16:40:01 +0530

    Fix: Deployment error fix in Hive.ts

--- src/hooks/Hive.ts
+++ src/hooks/Hive.ts
@@ -1,23 +1,22 @@
-import { useLayoutEffect, useState } from 'react'
-import {DHive} from '../const'
+import { useLayoutEffect, useState } from "react";
+import { DHive } from "../const";
 
 export function ResolveUsername(name) {
-    const [did, setDid] = useState(null)
-    useLayoutEffect(() => {
-        (async () => {
-            if(name.startsWith('did:')) {
-                setDid(name)
-            } else {
-                const accountInfo = (await DHive.database.getAccounts([name]))[0]
-                const json_metadata = JSON.parse(accountInfo.posting_json_metadata)
-                console.log(json_metadata)
-                console.log(json_metadata)
-                console.log(accountInfo)
-                setDid(json_metadata.did || null)
-            }
-        })()
-    }, [name])
+  const [did, setDid] = useState<string | null>(null);
+  useLayoutEffect(() => {
+    (async () => {
+      if (typeof name === 'string' && name.startsWith('did:')) {
+        setDid(name);
+      } else {
+        const accountInfo = (await DHive.database.getAccounts([name]))[0];
+        const json_metadata = JSON.parse(accountInfo.posting_json_metadata);
+        console.log(json_metadata);
+        console.log(json_metadata);
+        console.log(accountInfo);
+        setDid(json_metadata.did || null);
+      }
+    })();
+  }, [name]);
 
-    
-    return did;
+  return did;
 }
