3e3eda92f02c3554f9e699055dba9c25189c3df4
Geoffrey Casper <gcasper42@gmail.com>
2024-08-20 14:46:37 -0400

    disable hive login methods we aren't using

---
+++ src/components/Login/Hive/LoginOption.tsx
@@ -0,0 +1,48 @@
+import { Button, Flex, GridItem, Icon, VStack, Text } from "@chakra-ui/react";
+import { FaCheckCircle } from "react-icons/fa";
+import Image from "next/image";
+
+import styles from "./Hive.module.css";
+import type { LoginOptionName, LoginOptionType } from "./HiveModal";
+
+const DEFAULT_AUTH_OPTION: LoginOptionName = "Keychain";
+export const LOGIN_METHOD_FIELD = "loginMethod";
+
+export function LoginOption({ option }: { option: LoginOptionType }) {
+  return (
+    <GridItem>
+      <VStack>
+        <Flex gap={1} alignItems={"center"} position={"relative"}>
+          <Button
+            as="label"
+            aria-description={option.name}
+            colorScheme="gray"
+            w={125}
+            className={`${styles.container} ${
+              option.disabled ? styles.disabled : ""
+            }`}
+          >
+            <Image
+              alt={`${option.name} Logo`}
+              height={option.name === "Hivesigner" ? 20 : 25}
+              width={option.name === "Hivesigner" ? 20 : 25}
+              src={option.image}
+              objectFit="contain"
+            />
+            <Text fontSize={"sm"}>{option.name}</Text>
+            <input
+              type="radio"
+              name={LOGIN_METHOD_FIELD}
+              value={option.name}
+              defaultChecked={option.name === DEFAULT_AUTH_OPTION}
+              disabled={option.disabled}
+            />
+            <span className={styles.checkmark}>
+              <Icon className={styles.icon} as={FaCheckCircle} />
+            </span>
+          </Button>
+        </Flex>
+      </VStack>
+    </GridItem>
+  );
+}
---
+++ src/components/Login/Hive/LoginOptionContainer.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+
+export function LoginOptions({ children }: PropsWithChildren<{}>) {
+  return (
+    <Flex gap={2} w="full" wrap="wrap" justifyContent="center">
+      {children}
+    </Flex>
+  );
+}
