880d3d8f05dde8baea304668b27caf76d4981ba9
Geoffrey Casper <gcasper42@gmail.com>
2024-08-21 20:52:05 -0400

    SSR compatible chart on home page

--- src/components/Dashboard/ChartComponent.tsx
+++ src/components/Dashboard/ChartComponent.tsx
@@ -1,121 +1,81 @@
-import React, { useState } from 'react'
-import dynamic from 'next/dynamic'
+import React, { useMemo, useState } from 'react'
+import { Tooltip, Area, AreaChart, XAxis } from 'recharts'
 
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
+const WIDTH = 550
+const HEIGHT = 350
 
 const App = () => {
-  const [chartData, setChartData] = useState({
-    options: {
-      chart: {
-        id: 'trade',
-        toolbar: {
-          show: false,
-        },
-        sparkline: {
-          enabled: false,
-        },
-        padding: {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: false,
-      },
-      stroke: {
-        width: 0,
-      },
-      tooltip: {
-        enabled: false, // Enable tooltips
-      },
-      xaxis: {
-        labels: {
-          show: true,
-          offsetX: -10,
-          offsetY: 0,
-        },
-        categories: [
-          '',
-          'Nov 11',
-          'Nov 12',
-          'Nov 13',
-          'Nov 14',
-          'Nov 15',
-          'Nov 16',
-          '',
-        ],
-        axisBorder: {
-          show: false,
-        },
-        axisLabel: {
-          show: false,
-        },
-      },
-      yaxis: {
-        labels: {
-          show: false,
-          offsetX: 0,
-          offsetY: 0,
-        },
-        min: 0,
-        max: 100,
-        forceNiceScale: false,
-      },
-      grid: {
-        show: false,
-      },
-      fill: {
-        type: 'gradient',
-        colors: ['rgb(123, 138, 238)'],
-        gradient: {
-          shadeIntensity: 0.8,
-          opacityFrom: 0.4,
-          opacityTo: 0.7,
-          stops: [0, 100],
-        },
-      },
-      markers: {
-        size: 0, // size of the marker/circle
-        strokeWidth: 1, // border width of the circle
-        hover: {
-          size: 3, // size of the marker when hovered
-        },
-      },
-      annotations: {
-        xaxis: [
-          {
-            x: 'Nov 10',
-            label: {
-              text: 'Nov 10',
-              style: {
-                color: '#333',
-                background: 'transparent',
-              },
-            },
-          },
-        ],
-      },
-    },
-    series: [
-      {
-        name: 'series',
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  })
+  const [chartData, setChartData] = useState(
+    [
+      30, 40, 45, 50, 49, 60, 61, 64, 62, 66, 67, 67, 74, 82, 84, 87, 89, 91,
+    ].map((value, i) => ({
+      name: `Nov ${i + 1}`,
+      Hive: value,
+    })),
+  )
+
+  const xAxisTicks = useMemo(() => {
+    const mod = Math.ceil(chartData.length / 7)
+    return chartData.reduce(
+      (acc, { name }, i) => (
+        i % mod === mod - 1 && i !== chartData.length - 1
+          ? acc.push(name)
+          : void 0,
+        acc
+      ),
+      [] as string[],
+    )
+  }, [chartData])
 
   return (
-    <Chart
-      options={chartData.options}
-      series={chartData.series}
-      width="550"
-      type="area"
-    />
+    <AreaChart
+      // this stops hydration errors when using SSR
+      id="dashboard-chart"
+      width={WIDTH}
+      height={HEIGHT}
+      data={chartData}
+    >
+      <defs>
+        <linearGradient
+          id="colorUv"
+          x1="0"
+          y1="0"
+          x2="0"
+          y2="1"
+        >
+          <stop
+            offset="5%"
+            stopColor="#8884d8"
+            stopOpacity={0.8}
+          />
+          <stop
+            offset="95%"
+            stopColor="#8884d8"
+            stopOpacity={0}
+          />
+        </linearGradient>
+      </defs>
+      <Tooltip />
+      <style>{`
+        .recharts-text {
+          fill: #666;
+        }
+      `}</style>
+      <XAxis
+        stroke="transparent"
+        dataKey="name"
+        ticks={xAxisTicks}
+      />
+      <Area
+        // this stops hydration errors when using SSR
+        isAnimationActive={false}
+        type="monotone"
+        dataKey="Hive"
+        stroke="#8884d8"
+        fillOpacity={1}
+        fill="url(#colorUv)"
+      />
+    </AreaChart>
   )
 }
 
