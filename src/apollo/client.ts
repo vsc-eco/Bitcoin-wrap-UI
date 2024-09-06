c401d3f2d419de921e23f0b98463ca65aeb94f8c
Geoffrey Casper <gcasper42@gmail.com>
2024-09-06 07:25:27 -0400

    add env var for gql api uri

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+const API = process.env['NEXT_PUBLIC_VSC_NODE_URL'] || 'https://api.vsc.eco'
+
 export const client = new ApolloClient({
-  uri: 'https://api.vsc.eco/api/v1/graphql',
+  uri: API + '/api/v1/graphql',
   cache: new InMemoryCache(),
 })
