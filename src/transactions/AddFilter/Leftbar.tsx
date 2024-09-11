eca8f65f5b89b0451e52da6aed0261c03bfcb75e
piyushjha0409 <peeyush0409@gmail.com>
2024-09-11 20:52:18 +0530

    Feat: Made a separate component for the leftbar add filter modal

---
+++ src/transactions/AddFilter/Leftbar.tsx
@@ -0,0 +1,71 @@
+import { Box, Flex, Text, Icon, Switch } from '@chakra-ui/react'
+import React, { SetStateAction } from 'react'
+import { IconType } from 'react-icons/lib'
+import { BiSolidRightArrow } from "react-icons/bi";
+type Props = {
+  filterOptions: {
+    id: number
+    name: string
+    icon: IconType
+  }[]
+  selectedItem: number
+  setSelectedItem: React.Dispatch<SetStateAction<number>>
+}
+
+const Leftbar = ({ filterOptions, selectedItem, setSelectedItem }: Props) => {
+  return (
+    <Flex
+      flexDirection={'column'}
+      width={'38%'}
+      backgroundColor={'#fbfcfd'}
+      boxShadow={'1px 0px 0px #f0f1f4'}
+    >
+      <Flex
+        w="full"
+        justifyContent={'center'}
+        alignItems={'center'}
+        paddingX={'4px'}
+        paddingY={'16px'}
+        fontSize={'16px'}
+        color={'#535461'}
+        gap={2}
+      >
+        <Text size={'sm'}>My transactions</Text>
+        <Switch size={'sm'} />
+      </Flex>
+      {filterOptions.map(option => (
+        <Flex
+          key={option.id}
+          w="full"
+          bgColor={selectedItem === option.id ? '#f3f4f7' : 'transparent'}
+          alignItems="center"
+          marginTop={'2px'}
+          paddingY={'2px'}
+          paddingX="12px"
+          borderRadius={'12px'}
+          marginRight="2px"
+          cursor="pointer"
+          onClick={() => setSelectedItem(option.id)}
+        >
+          <Icon
+            as={option.icon}
+            color="#535461"
+            fontSize={'small'}
+            marginRight={'12px'}
+          />
+          <Text
+            color={'#535461'}
+            cursor={'pointer'}
+            fontWeight={400}
+            lineHeight={'24px'}
+            fontSize={'15px'}
+          >
+            {option.name}
+          </Text>
+        </Flex>
+      ))}
+    </Flex>
+  )
+}
+
+export default Leftbar
