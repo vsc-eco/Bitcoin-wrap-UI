e35567cabde8f391fa02d6a6ad57855097e835f7
piyushjha0409 <peeyush0409@gmail.com>
2024-09-26 17:13:41 +0530

    Fix: added some more shades

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,14 +5,26 @@ export const colors: Colors = {
   black: '#000',
   white: '#fff',
 
+  cream: {
+    50: '#fbfcfd',
+    100: '#f0f1f4',
+    150: '#e3e6fc',
+    200: '#c5caf0',
+  },
+
   gray: {
     50: '#f7fafc',
     500: '#A0AEC0', // Light gray
     600: '#718096', // Darker gray for light mode
+    650: '#9a9a9a',
+    700: '#73737a',
+    800: '#75757d',
+    850: '#535461',
     900: '#171923',
   },
   indigo: {
     900: '#5266eb', // Indigo dark
+    800: '#7b8aee',
     700: '#a6aff7', // Indigo medium
     500: '#c3cdfa', // Indigo light
   },
