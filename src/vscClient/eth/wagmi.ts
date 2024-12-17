793158f6db1d0cc15d08cc45f63e91e1d7219455
Matthew Trent <matthew.rl.trent@gmail.com>
2024-12-17 10:50:08 -0800

    refactor: moves cbor to eip712 converting files into eth dir

--- src/vscClient/eth/wagmi.ts
+++ src/vscClient/eth/wagmi.ts
@@ -2,7 +2,7 @@ import { encode } from '@ipld/dag-cbor'
 import { encodePayload } from 'dag-jose-utils'
 import type { Signer } from '../client'
 import { Config, signTypedData } from '@wagmi/core'
-import { convertCBORToEIP712TypedData } from '../cbor_to_eip712_converter'
+import { convertCBORToEIP712TypedData } from './cbor_to_eip712_converter'
 
 export const wagmiSigner = (async (txData, _, config: Config) => {
   const types = convertCBORToEIP712TypedData(
